3894b5f9521385d47ec41c9d92748b08ec421fa6
Abhi <sr7324625@gmail.com>
2023-04-01 19:12:16 +0530

    trial

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const mongoose = require("mongoose");
 const data = require("./db");
 const data2 = require("./db");
 
-
-console.log("connected successfully")
+console.log("connected");
 
 // db = mongoose.connect("mongodb://localhost:27017/aqwave");
 // db = mongoose.connect("mongodb://localhost:27017/aqwave"); important
@@ -16,29 +15,25 @@ console.log("connected successfully")
 
 // var db = process.env.DATABASE
 
-db = mongoose.connect("mongodb+srv://Abhijeet2109:abhijeet21o9@cluster0.ohwhhle.mongodb.net/?retryWrites=true&w=majority")
+db = mongoose.connect(
+  "mongodb+srv://Abhijeet2109:abhijeet21o9@cluster0.ohwhhle.mongodb.net/?retryWrites=true&w=majority"
+);
 //import path from 'path'
-db.then(()=>{
-  console.log("successfully connected to database")
-}).catch((err)=>{
-  console.log('connection fail to the database')
-})
-
+db.then(() => {
+  console.log("successfully connected to database");
+}).catch((err) => {
+  console.log("connection fail to the database");
+});
 
 const port = 3000;
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
+  console.log("i am here");
+  res.sendFile(path.join(__dirname, "/index3.html"));
 });
-
-app.get("/info/:slug", (req, res) => {
-  // res.sendFile(path.join(__dirname,"/index.html"))
-  console.log(req.params.slug);
-  res.send(req.params.slug.split(""));
-});
+app.use(express.static(path.join(__dirname, "")));
 
 app.get("/coming", (req, res) => {
   // res.sendFile(path.join(__dirname,"/index.html"))
@@ -68,7 +63,7 @@ app.use("/contact", async (req, res) => {
     let p = await d2.save();
     res.sendFile(path.join(__dirname, "/index2.html"));
     // console.log(req.body.fname)
-    console.log("success")
+    console.log("success");
   } catch (e) {
     console.log(e);
   }
@@ -92,3 +87,9 @@ app.post("/signup", async (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+// app.get("/info/:slug", (req, res) => {
+//   // res.sendFile(path.join(__dirname,"/index.html"))
+//   console.log(req.params.slug);
+//   res.send(req.params.slug.split(""));
+// });
